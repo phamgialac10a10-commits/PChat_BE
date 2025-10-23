5003bfd50ca3465658132789c7eb6ba6e240abfc
PeanutEz <phamlac10@gmail.com>
2025-10-23 09:55:05 +0700

    Lac

---
+++ src/config/mongo.config.ts
@@ -0,0 +1,27 @@
+import { MongooseModule } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+
+// ✅ Ưu tiên lấy từ .env, nếu không có thì fallback
+const mongoUri =
+  process.env.MONGO_URI ||
+  'mongodb+srv://laclac:lacpham2005@pchat.igp8a1c.mongodb.net/?retryWrites=true&w=majority&appName=PChat';
+
+if (!mongoUri) {
+  throw new Error('❌ Missing MONGO_URI in environment variables');
+}
+
+
+
+// ✅ Kết nối MongoDB
+export const MongoConfig = MongooseModule.forRoot(mongoUri, {
+  connectionName: 'mongoConnection', 
+});
+console.log('✅ MongoDB connected successfully!');
+
+// mongoose.connection.on('connected', () => {
+//   console.log('✅ MongoDB connected successfully!');
+// });
+
+// mongoose.connection.on('error', (err) => {
+//   console.error('❌ MongoDB connection error:', err.message);
+// });
---
+++ src/config/mysql.config.ts
@@ -0,0 +1,16 @@
+import { ConfigService } from '@nestjs/config';
+import * as mysql from 'mysql2/promise';
+
+export async function createMySQLConnection(config: ConfigService) {
+  const pool = await mysql.createPool({
+    host: config.get<string>('MYSQL_HOST'),
+    user: config.get<string>('MYSQL_USER'),
+    password: config.get<string>('MYSQL_PASSWORD'),
+    database: config.get<string>('MYSQL_DATABASE'),
+    waitForConnections: true,
+    connectionLimit: 10,
+  });
+  console.log(`✅ MySQL connected: ${config.get('MYSQL_DATABASE')}`);
+  return pool;
+}
+
